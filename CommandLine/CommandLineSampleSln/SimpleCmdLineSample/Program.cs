5218c2f06152cd94d8a6c75dbe345a8c5dcd82d7
Trelston Moraes <trelston@gmail.com>
2020-10-14 13:37:21 +0200

    Command Line Project

---
+++ CommandLine/CommandLineSampleSln/SimpleCmdLineSample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.DragonFruit;
+    
+
+namespace SimpleCmdLineSample
+{
+    class Program
+    {
+        static void Main(string input, string output)
+        {
+            Console.WriteLine("Hello World!" + input);
+            Console.WriteLine("Hello World!" + output);
+
+        }
+    }
+}
