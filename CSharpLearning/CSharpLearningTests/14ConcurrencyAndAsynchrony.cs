53a2f21cf356f9d5b2549206c7db0b7e5151302a
Trelston Moraes <trelston@gmail.com>
2021-04-09 07:54:05 +0200

    Test

--- CSharpLearning/CSharpLearningTests/14ConcurrencyAndAsynchrony.cs
+++ CSharpLearning/CSharpLearningTests/14ConcurrencyAndAsynchrony.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Threading;
 using NUnit.Framework;
 
 namespace CSharpLearningTests
@@ -10,8 +12,37 @@ namespace CSharpLearningTests
         }
 
         [Test]
-        public void Test1()
+        public void CreatingAThreadUsingTheThreadStartDelegate()
         {
+            Thread t = new Thread(() =>
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    Console.Write('x');
+                } 
+            });
+            t.Start();
+            for (int i = 0; i < 1000; i++)
+            {
+               Console.Write('y'); 
+            }
+            
+            Assert.Pass();
+        }
+
+        [Test]
+        public void WaitForAnotherThreadToEndByCallingJoin()
+        {
+            Thread t = new Thread(() =>
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    Console.WriteLine('y');
+                }
+            });
+            t.Start();
+            t.Join();
+            Console.WriteLine("Thread t has ended");
             Assert.Pass();
         }
     }
